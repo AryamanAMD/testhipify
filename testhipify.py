22ac3d464b47697bb5ff09aa6d2eabe0d3dc600d
Aryaman Mishra <aryaman.mishra73@gmail.com>
2022-10-10 00:00:19 +0530

    Update testhipify.py

--- testhipify.py
+++ testhipify.py
@@ -20,50 +20,22 @@ def getListOfFiles(dirName):
 def ftale(x):
 	x=x.replace('"', '')
 	p=os.path.dirname(x)
+	q=os.path.basename(x)
 	p=p.replace("\\","/")
-	os.system("cd "+p)
-	##print("cd "+p)
-	command="/opt/rocm/bin/hipify-perl "+p+"/"+os.path.basename(x)+" > "+p+"/"+os.path.basename(x)+".hip"
-	os.system(command)
-	##command="/opt/rocm/bin/hipcc -I /home/taccuser/cuda-samples/Common -I /home/taccuser/cuda-samples/Common/GL -I /home/taccuser/cuda-samples/Common/UtilNPP -I /home/taccuser/cuda-samples/Common/data -I /home/taccuser/cuda-samples/Common/lib/x64 "+p+"/"+os.path.basename(x)+".hip"
-	command="/opt/rocm/bin/hipcc -I /home/taccuser/testhipify/src/samples/Common -I /home/taccuser/testhipify/src/samples/Common/GL -I /home/taccuser/testhipify/src/samples/Common/UtilNPP -I /home/taccuser/testhipify/src/samples/Common/data -I /home/taccuser/testhipify/src/samples/Common/lib/x64 "+p+"/"+os.path.basename(x)+".hip"
-	os.system(command)
-	##print(command)
-	##command="/opt/rocm/bin/hipcc -I /home/taccuser/cuda-samples/Common "+p+"/"+os.path.basename(x)+".hip -o "+p.rsplit('.', 1)[0]+".out"
-	os.system(command)
-	##print(command)
-	##command="/opt/rocm/bin/hipcc -use-staticlib "+p+"/"+os.path.basename(x)+".hip -o "+p.rsplit('.', 1)[0]+".out.static"
-	##os.system(command)
-	##print(command)
-	##command="cd "+p
-	##os.system(command)
-	##command="./"+os.path.basename(x)
-	##os.system(command)
-	##print(command)
-	##command="hipcc "+os.path.basename(x)+" -o "+ square.out"
-	##command="hipcc -use-staticlib  square.cpp -o square.out.static"
-	##command="./square.out"
-	##print(command)
+	with open(r'{q}', 'r') as fp:
+		lines = fp.readlines()
+		for row in lines:
+			word = '#include <GL'
+			if row.find(word) == -1:
+				os.system("cd "+p)
+				os.system("sed 's/checkCudaErrors/HIPCHECK/g' "+q)
+				command="/opt/rocm/bin/hipify-perl "+p+"/"+q+" > "+p+"/"+q+".hip"
+				os.system(command)
+				command="/opt/rocm/bin/hipcc -I /home/taccuser/testhipify/src/samples/Common -I /home/taccuser/testhipify/src/samples/Common/GL -I /home/taccuser/testhipify/src/samples/Common/UtilNPP -I /home/taccuser/testhipify/src/samples/Common/data -I /home/taccuser/testhipify/src/samples/Common/lib/x64 "+p+"/"+os.path.basename(x)+".hip"
+				os.system(command)
 	
 
 def fall(y):
-	"""
-	dir_list=os.listdir(y)
-	for i in dir_list:
-		if i.lower().endswith('.cu') or i.lower().endswith('.cpp'):
-			command="perl hipify-perl "+i+" > "+i+".hip"
-			os.system(command)
-			
-	pattern=["*.cu","*.cpp"]
-	for path, subdirs, files in os.walk(y):
-		for name in files:
-			if fnmatch(name, pattern):
-				##print(os.path.join(path, name))
-				f=os.path.join(path, name)
-				command="perl hipify-perl "+f+" > "+f+".hip"
-				##os.system(command)
-				print(command)
-				"""
 	y=y.replace('"', '')
 	listOfFiles=getListOfFiles(y)
 	for elem in listOfFiles:
