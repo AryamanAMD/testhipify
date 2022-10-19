23b47f12498fd25e4f62fcab70ae02234af8b484
AryamanAMD <aryamanatamd@gmail.com>
2022-10-19 16:56:12 +0530

    Update testhipify.py

--- testhipify.py
+++ testhipify.py
@@ -58,9 +58,9 @@ def ftale(x):
 	textToReplace="HIPCHECK"
 	fileToSearch=p+"/"+q+".hip"
 	textToSearch1="hipProfilerStart"
-	textToReplace1="rocprofiler_startt_queue_callbacks"
+	textToReplace1="rocprofiler_start_queue_callbacks"
 	textToSearch2="hipProfilerStop"
-	textToReplace2="rocprofiler_stopt_queue_callbacks"
+	textToReplace2="rocprofiler_stop_queue_callbacks"
 	tempFile=open(fileToSearch,'r+')
 	for line in fileinput.input(fileToSearch):
 		tempFile.write(line.replace(textToSearch,textToReplace))
