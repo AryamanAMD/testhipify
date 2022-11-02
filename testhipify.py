fd228e21ebc7c626e2e66a98145b382ef305889a
AryamanAMD <aryamanatamd@gmail.com>
2022-11-02 15:17:52 +0530

    Update testhipify.py

--- testhipify.py
+++ testhipify.py
@@ -122,7 +122,7 @@ def generate(x):
 	tempFile.close()
 
 def apply_patches():
-	command='git apply --reject --whitespace=fix src/patches/patch_for_0_introduction.patch_utf16.patch'
+	command='git apply --reject --whitespace=fix src/patches/*.patch'
 	print(command)
 	os.system(command)
 
