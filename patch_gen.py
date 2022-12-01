5ed6f4964f5b3ad7d591b3d3a166331310d5aa3a
AryamanAMD <aryamanatamd@gmail.com>
2022-12-01 12:26:19 +0530

    Update patch_gen.py

---
+++ patch_gen.py
@@ -0,0 +1,41 @@
+import os
+def getListOfFiles(dirName):
+    listOfFile=os.listdir(dirName)
+    allFiles=list()
+    for entry in listOfFile:
+        fullPath=os.path.join(dirName, entry)
+        if os.path.isdir(fullPath):
+            allFiles=allFiles+getListOfFiles(fullPath)
+        else:
+            allFiles.append(fullPath)
+                
+    return allFiles
+
+
+def generate(x):
+    x=x.replace('"', '')
+    x=x.replace("\\","/")
+    #print(os.path.splitext("/path/to/some/file.txt")[0])
+    command="hipify-perl "+x+" > "+x[0]+"_hipified.cpp"
+    print(command)
+    os.system(command)
+    replace_words(x,'#include <helper_cuda.h>','#include "helper_cuda_hipified.h"')
+    replace_words(x,"#include <helper_functions.h>",'#include "helper_functions.h"')
+	
+	  
+def generate_all(y):
+	y=y.replace('"', '')
+	listOfFiles=getListOfFiles(y)
+	for elem in listOfFiles:
+		if elem.endswith('_gold.cpp') or elem.equals('main.cpp'):  ##or elem.endswith('.cpp') 
+                    generate(elem)
+
+
+def replace_words(x,search_text,replace_text):
+    with open(x[0]+"_hipified.cpp", 'r') as file:
+        data = file.read()
+        data = data.replace(search_text, replace_text)
+    with open(x[0]+"_hipified.cpp", 'w') as file:
+        file.write(data)
+					    
+generate_all('src/samples/Samples')                        
